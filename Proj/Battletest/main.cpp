e97c458ef190f293f3b6010df71a3a55386de6f5
Hassan-Farhat <hfarhat@student.rcc.edu>
2016-10-15 23:47:27 -0700

    Signed-off-by: Hassan-Farhat <hfarhat@student.rcc.edu>

--- Proj/Battletest/main.cpp
+++ Proj/Battletest/main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char** argv) {
     srand(static_cast <unsigned int> (time(0)));
     
     //Declare variables:
-    int monMove, pMove, health=15, mHealth = 3, hit;  
+    int monMove, pMove, health=15, mHealth = 3, hit, dodge;  
     int agility = 5, attck = 5, pAglty, pAttck, points = 4; 
     string input, countAtt;
     
@@ -50,19 +50,36 @@ int main(int argc, char** argv) {
             cout << "OUT OF POINTS" << endl;
         }
     } while (points > 0);
+    cout << "You have " << attck << " attack and " << agility << " agility." << endl;
+    
+    //Modifiers:
+    pAglty = 100 - (agility * 10);
+    pAttck = 100 - (attck * 5);
     
     //Battle:
     do{
-        cout << "Input you move (attack or dodge):"<<endl;
-        cin >> input;
-        if (input == "dodge"){ pMove = 0;
+        do {
+            cout << "Input you move (attack or dodge):"<<endl;
+            cin >> input;
+        }while (!(input == "dodge") && !(input == "Dodge") && !(input == "attack") && !(input == "Attack"));
+        if (input == "dodge" || input == "Dodge"){
+            dodge = rand()%90 + 11;
+            if (dodge >= pAglty){
+                cout << "Your dodge roll was " << dodge << "; enough to dodge." << endl;
+                pMove = 0;
+            }
+            else {
+                cout << "Your dodge roll was " << dodge << endl;
+                cout << "YOU FAILED YOUR DODGE YOU MUST ATTACK" << endl;
+                pMove = 1;
+            }
         }
         else pMove = 1;
         monMove = rand()%9 + 1;
         int mMove = monMove%4;
         if ( (mMove == 1 || mMove == 2 || mMove == 3) && pMove == 0){
             cout << "YOU DODGED !" << endl;
-            cout << "DO YOU WANT TO TRY TO ATTACK ? (This is risky) (yes or no)" << endl;
+            cout << "DO YOU WANT TO TRY TO ATTACK ? (This is risky) (enter yes to attack)" << endl;
             cin >> countAtt;
             if (countAtt == "Yes" || countAtt == "yes") {
                 int cAtt = rand()%90 + 11;
@@ -79,34 +96,40 @@ int main(int argc, char** argv) {
                     mHealth -= 2; 
                 }
             }
-            cout << "Your health = " << health << endl << "Monster's health = " << mHealth << endl;
+            cout << endl << "Your health = " << health << endl << "Monster's health = " << mHealth << endl;
         }
         else if ((mMove == 1 || mMove == 2 || mMove == 3) && pMove == 1){
             cout << "YOU HIT THE MONSTER AND IT HIT YOU" << endl;
             hit = rand()%90 + 11;
             health -= 2;
-            if (hit >= 80){
+            if (hit >= pAttck){
+                cout << "Your critical hit roll was " << hit << endl;
                 cout << "IT WAS A CITICAL HIT" << endl;
                 mHealth -= 3;
             }
             else {
+                cout << "Your critical hit roll was " << hit << endl;
                 mHealth -= 1;
             }
-            cout << "Your health = " << health << endl << "Monster's health = " << mHealth << endl;
+            cout << endl << "Your health = " << health << endl << "Monster's health = " << mHealth << endl;
         }
         else {
             cout << "THE MONSTER IS DAZED ! YOU ATTACK FOR FREE" << endl;
             hit = rand()%90 + 11;
-            if (hit >= 80){
+            if (hit >= pAttck){
+                cout << "Your critical hit roll was " << hit << endl;
                 cout << "IT WAS A CITICAL HIT" << endl;
                 mHealth -= 3;
             }
             else {
+                cout << "Your critical hit roll was " << hit << endl;
                 mHealth -= 1;
             }
-            cout << "Your health = " << health << endl << "Monster's health = " << mHealth << endl;
+            cout << endl << "Your health = " << health << endl << "Monster's health = " << mHealth << endl;
         }
+        if (mHealth < 0) {cout << "YOU KILLED THE MONSTER" << endl;}
         }while (mHealth > 0 && health > 0);
+        
     return 0;
 }
 
